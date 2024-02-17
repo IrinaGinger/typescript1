f1e18af0cb57286f616108682cc9c96cd70a2a24
Irina Kriuchkova <ivk2005@yandex.ru>
2024-02-17 13:47:35 +0300

    Main task done

--- src/ts/service/Cart.ts
+++ src/ts/service/Cart.ts
@@ -10,4 +10,18 @@ export default class Cart {
     get items(): Buyable[] {
         return [...this._items]; 
     }
+
+    sumWithoutDiscount(): number {
+        let sum: number = this._items.reduce((acc, currentValue) => acc + currentValue.price, 0);
+        return sum;
+    }
+
+    sumWithDiscount(discount: number): number {
+        let sum: number = this._items.reduce((acc, currentValue) => acc + currentValue.price, 0) * (1 - discount / 100);
+        return sum;
+    }
+
+    delete(id : number) : void {
+        this._items = this._items.filter((item) => item.id !== id);
+    }
 }
